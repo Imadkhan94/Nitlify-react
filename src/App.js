64aad3c6b6ec82a524794213cfb6023dcf5c7352
Imadkhan <imadkhanmohmand@gmail.com>
2020-10-29 20:32:26 +0500

    update netlifyreact

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello world from react</h1>
+      <h1>Hello world from react </h1>
+      <h1>This is imad khan</h1>
     </div>
   );
 }
